96904061bc03417f5c0ba7510334eff576b4ee33
EladKrief <85412746+EladKrief@users.noreply.github.com>
2022-11-08 23:17:43 +0200

    Add files via upload

---
+++ loop_task/script.js
@@ -0,0 +1,10 @@
+let numbers = [];
+function getUserNumbers() {
+  for (i = 0; i < 10; i++) {
+    numbers.push(prompt("Please Enter a Number"));
+  }
+  console.log(numbers);
+}
+numbers.forEach(getUserNumbers);
+
+getUserNumbers();
